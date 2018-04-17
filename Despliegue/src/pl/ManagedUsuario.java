2c34993781379ec14bf8c970e2272004ff1de815
sergio <sergio@sergio-virtual-machine>
2018-04-17 21:51:11 +0200

    Se han añadido la pagina de Inicio y una pagina donde pedir coordenadas para luego mostrar la ruta hasta las motos

--- Despliegue/src/pl/ManagedUsuario.java
+++ Despliegue/src/pl/ManagedUsuario.java
@@ -13,79 +13,105 @@ import dl.Usuario;
 
 @Named
 @RequestScoped
-public class ManagedUsuario implements Serializable{
-
+public class ManagedUsuario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Usuario cliente = new Usuario();
-	private boolean checkInsert=false;
-	private boolean checkCopy=false;
-	
-	
+	private boolean checkInsert = false;
+	private boolean checkCopy = false;
+
 	public String getNombre() {
 		return cliente.getNombre();
 	}
+
 	public void setNombre(String nombre) {
 		cliente.setNombre(nombre);
 	}
+
 	public String getApellido() {
 		return cliente.getApellido();
 	}
+
 	public void setApellido(String apellido) {
 		cliente.setApellido(apellido);
 	}
+
 	public String getCorreo() {
 		return cliente.getCorreo();
 	}
+
 	public void setCorreo(String correo) {
 		cliente.setCorreo(correo);
 	}
+
 	public String getPassword() {
 		return cliente.getPassword();
 	}
+
 	public void setPassword(String password) {
 		cliente.setPassword(password);
 	}
-	
-	public void add(){
-		
-		if(buscaUsuario()){
-		ClientBuilder
-		.newClient()
-		.target("http://localhost:8080/TAP/rest/servicio/")
-		.path("anadir")
-		.request()
-		.post(Entity.json(cliente));
-		this.checkInsert=true;
-		}else{
-			this.checkCopy=true;
+
+	public void add() {
+
+		if (buscaUsuario()) {
+			ClientBuilder.newClient().target("http://localhost:8080/TAP/rest/servicio/").path("anadir").request()
+					.post(Entity.json(cliente));
+			this.checkInsert = true;
+		} else {
+			this.checkCopy = true;
 		}
 	}
 
-	public boolean buscaUsuario () {
-		
-			LeerFicheroXML leer =new LeerFicheroXML();
-			ListaUsuarios lista=new ListaUsuarios();
-			boolean check=true;
-			
-			lista.setLista(leer.leer().getLista());
-			
-			for (Usuario user : lista.getLista()) {
-				if(user.getCorreo().contentEquals(user.getCorreo())){
-					check=false;
-				}
+	public boolean buscaUsuario() {
+
+		// El boolean esta puesto asi para renderizar la respuesta
+
+		LeerFicheroXML leer = new LeerFicheroXML();
+		ListaUsuarios lista = new ListaUsuarios();
+		boolean check = true;
+
+		lista.setLista(leer.leer().getLista());
+
+		for (Usuario user : lista.getLista()) {
+			if (user.getCorreo().contentEquals(this.cliente.getCorreo())) {
+				check = false;
 			}
-			return(check);
-	
+		}
+		return (check);
+
 	}
-	
-	public boolean checkInsert(){
-		
-		return(this.checkInsert);
+
+	public boolean checkInsertCorreo() {
+		// Mira si se ha insertado el correo en el fichero
+		// Si esto se cumple se devuelve True
+		return (this.checkInsert);
 	}
-	
-	public boolean checkCopy(){
-		
-		return(this.checkCopy);
+
+	public boolean checkCopyCorreo() {
+		// Mira si hay un correo igual en el fichero
+		// Si esto se cumple se devuelve True
+		return (this.checkCopy);
 	}
+
+	
+
+	public String checkRegistro() {
+		// Busca si el usuario y la contraseña estan en el fichero
+
+		LeerFicheroXML leer = new LeerFicheroXML();
+		ListaUsuarios lista = new ListaUsuarios();
+		String check ="false";
+
+		lista.setLista(leer.leer().getLista());
+
+		for (Usuario user : lista.getLista()) {
+			if (user.getCorreo().contentEquals(cliente.getCorreo()) && user.getPassword().contentEquals(cliente.getPassword())) {
+				check="PaginaActividad";
+			}
+		}
+
+		return(check);
+	}
+
 }
